a3ea686e875323043ee42f9a1a8e2a572441751b
benjamin.thos <benjamin.thos@epita.fr>
2023-11-16 15:47:30 +0100

    add button on wifi

--- wifi.sh
+++ wifi.sh
@@ -4,10 +4,15 @@ state=$(nmcli device status | sed -n 's/.* wifi [ ]*\([a-z]*\).*/\1/p')
 connection=$(nmcli device status | sed -n 's/.* wifi [ ]*[a-z]*[ ]*\([^ ]*\)[ ]*/\1/p')
 state_eth=$(nmcli device status | sed -n 's/.* ethernet [ ]*\([a-z]*\).*/\1/p')
 
+if [ $button ]; then
+    exec alacritty -e nmtui
+fi
+
 TEXT_COLOR=#282828
 if [ $state_eth = connected ]; then 
-	echo " 󰈀 Ethernet" 
+	echo " 󰈀 Ethernet " 
 	echo " 󰈀 " 
+	BACK_COLOR=#5a189a
 elif [ $state = connected ];then 
 	echo " 󰖩  $connection " 
 	echo " 󰖩 " 
